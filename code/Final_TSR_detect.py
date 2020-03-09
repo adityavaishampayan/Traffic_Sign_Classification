067f978ba1b8336ca4f1cc1b19de9cc1bbf6d5ea
ADITYA VAISHAMPAYAN <aditya1.vaishampayan@gmail.com>
2020-03-09 18:50:16 -0400

    Add files via upload

--- code/Final_TSR_detect.py
+++ code/Final_TSR_detect.py
@@ -17,33 +17,22 @@ import glob
 import pickle
 from sklearn import datasets
 from sklearn import svm
-import skimage
-#from skimage.feature import hog
-from sklearn import svm
-from sklearn.metrics import classification_report,accuracy_score
-from skimage import data, exposure
-import matplotlib.pyplot as plt
-import numpy as np
-import pickle
-from scipy.misc.pilutil import imread
 
 
 
-
-imgList = glob.glob("/media/aditya/EDUCATION/UMCP_robotics/SPRING_2019/ENPM_673/projects/PROJECT_6/TSR-20190510T203625Z-001/TSR/input/*")
+imgList = glob.glob("/media/nakul/DATA/Ubuntu_UMD_Courses/Perception/Project-6/TSR/input/*")
 imgList.sort()
 #imgList = imgList[80:]
 hog = cv2.HOGDescriptor()
-filename = open("morning_classifier.sav",'rb')
+filename = open("SVM_Classifier.sav",'rb')
 clf = pickle.load(filename)
 
 for img in imgList:
     regions=[] 
     # ======== Traffic sign detection using HSV ===================== #
-    image = skimage.io.imread(img)    
-    #image = cv2.imread(img)
+    image = cv2.imread(img)
     #image = cv2.fastNlMeansDenoisingColored(image,None,10,10,7,21)
-    tmpl = glob.glob("/templates/*")
+    tmpl = glob.glob("/media/nakul/DATA/Ubuntu_UMD_Courses/Perception/Project-6/TSR/templates/*")
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     
     # ======= Traffic sign detection using MSER Algorithm ============= #
@@ -66,8 +55,8 @@ for img in imgList:
             resized = cv2.resize(regions, (64, 64)) 
             h = hog.compute(resized)
             h = h.reshape(1, -1)
-            y_pred = clf.predict(h)
-            class_probabilities = clf.predict_proba(h)
+            y_pred = loaded_model.predict(h)
+            class_probabilities = loaded_model.predict_proba(h)
             if np.amax(class_probabilities) >= 0.75:
                 
                 template = cv2.imread(tmpl[y_pred])            
@@ -89,28 +78,12 @@ for img in imgList:
         #print('ar', w/h)
         if w/h >= 0.5 and w/h <= 1.5 and cv2.contourArea(cnt) >= 400 and cv2.contourArea(cnt) <= 4000:
             blue = cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
-            regions.append(image[y: y+h, x: x+w])
-            regions = np.array(regions)
+              regions.append(image[y: y+h, x: x+w])
             resized = cv2.resize(regions, (64, 64)) 
-            
-#==============================================================================
-#             image = imread(regions)
-#             image_resized = skimage.transform.resize(image, (64, 64), anti_aliasing=True)
-#             
-#             
-#             fd, hog_image = hog(image_resized, orientations=8,
-#                         pixels_per_cell=(4, 4),
-#                         cells_per_block=(2, 2),
-#                         block_norm= 'L2-Hys',
-#                         visualize=False,
-#                         transform_sqrt=True)
-#==============================================================================
-                        
-                        
             h = hog.compute(resized)
             h = h.reshape(1, -1)
-            y_pred = clf.predict(h)
-            class_probabilities = clf.predict_proba(h)
+            y_pred = loaded_model.predict(h)
+            class_probabilities = loaded_model.predict_proba(h)
             if np.amax(class_probabilities) >= 0.75:
                 
                 template = cv2.imread(tmpl[y_pred])            
